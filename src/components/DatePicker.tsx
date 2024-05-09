1c90e74cf4a1e1aa729689536352a4151f810bc8
Insaram Alam <alaminsaram92@gmail.com>
2024-05-09 23:49:40 +0500

    date picker

---
+++ src/components/DatePicker.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity, View, ViewStyle } from "react-native";
+import { TextNormal, TextSmall, TextSmaller } from "./AppText";
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+interface DatePickerProps {
+  value: any;
+  onChange: (arg: any) => void;
+  label?: string;
+  placeholder?: string;
+  containerStyle?: ViewStyle;
+  dateInputContainerStyle?: ViewStyle;
+  error?: string;
+  isTime?: true;
+}
+
+const DatePicker: React.FC<DatePickerProps> = (props) => {
+  const {
+    value,
+    onChange,
+    label,
+    placeholder,
+    error,
+    containerStyle,
+    isTime,
+    dateInputContainerStyle,
+  } = props;
+
+  const [date, setDate] = useState(value ?? new Date());
+  const [mode, setMode] = useState(isTime ? "time" : "date");
+  const [show, setShow] = useState(false);
+
+  const onHandleChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+    onChange(currentDate)
+  };
+
+  const showMode = () => {
+    setShow(true);
+    setMode(mode);
+  };
+
+  return (
+    <>
+      <View style={[styles.container, containerStyle]}>
+        {label && (
+          <TextSmall color="#BBBBBB" textStyle={styles.label}>
+            {label}
+          </TextSmall>
+        )}
+        <TouchableOpacity
+          style={[styles.dateContainerStyle, dateInputContainerStyle]}
+          onPress={showMode}
+        >
+          <TextNormal color={value?"white":"#BBBBBB"}>
+            {value?.toLocaleDateString() ?? placeholder ?? "Please Select Date"}
+          </TextNormal>
+          {show && (
+            <DateTimePicker
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              onChange={onHandleChange}
+            />
+          )}
+        </TouchableOpacity>
+      </View>
+      {error && (
+        <TextSmaller bold color={"red"}>
+          {"* "}
+          {error}
+        </TextSmaller>
+      )}
+    </>
+  );
+};
+
+export default DatePicker;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    width: "100%",
+  },
+  label: {},
+  dateContainerStyle: {
+    borderBottomWidth: 1,
+    borderColor: "white",
+    width: "100%",
+    alignSelf: "center",
+    height: 40,
+    justifyContent: "center",
+  },
+  textInput: {
+    fontSize: 16,
+    flex: 1,
+    color: "white",
+  },
+});
--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Controller } from "react-hook-form";
 import { StyleSheet, View } from "react-native";
 import Input from "./input";
+import DatePicker from "./DatePicker";
 
 interface FieldProps {
   id: string;
@@ -10,21 +11,30 @@ interface FieldProps {
 }
 
 const Fields: React.FC<FieldProps> = (props) => {
-  const { id, control, name } = props;
+  const { id, control, name, type } = props;
   return (
     <Controller
       control={control}
       name={name}
-      render={({ field: { value, onChange }, fieldState }) => (
-        <View key={id}>
-          <Input
-            value={value}
-            onChangeText={(val: string) => onChange(val)}
-            error={fieldState?.error?.message}
-            {...props}
-          />
-        </View>
-      )}
+      render={({ field: { value, onChange }, fieldState }) => {
+        if (type == "date") {
+          return (
+            <View key={id}>
+              <DatePicker value={value} onChange={onChange} {...props} />
+            </View>
+          );
+        }
+        return (
+          <View key={id}>
+            <Input
+              value={value}
+              onChangeText={(val: string) => onChange(val)}
+              error={fieldState?.error?.message}
+              {...props}
+            />
+          </View>
+        );
+      }}
     />
   );
 };
